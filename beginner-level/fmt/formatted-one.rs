c38b6c185f50cfaec3a219f67888ab21dabc901b
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-09-18 23:58:11 +0530

    Print function

---
+++ beginner-level/fmt/formatted-one.rs
@@ -0,0 +1,13 @@
+// https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=6673a231de794b58dd32baa48643791e
+fn main() {
+    print!("Well known message!!\n\nHello World!!");
+    
+}
+
+/*
+_Output_:-
+Well known message!!
+
+Hello World!!
+
+*/
